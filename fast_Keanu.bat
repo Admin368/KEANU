43385da5fb031e2280f5e855ff643de9aa5decfe
Admin368 <58757588+Admin368@users.noreply.github.com>
2019-12-18 01:17:11 +0800

    fast keanu "Launcher" Compatibility

--- fast_Keanu.bat
+++ fast_Keanu.bat
@@ -9,26 +9,11 @@ set Keanu.Speech=OFF
 set FullScreen=Yes
 set MQTT.Request=False
 set NodeRed.Request=False
-
-:Launcher_Essentials
-title %Launcher% %Keanu_Name% %version%
-echo %keanu.date.time% %Launcher% calling [KeanuLocation.bat] >>%access.logs%
-call location.bat
-
-
-:Launcher_Log
-echo %keanu.date.time% Launcher started [%launcher%] >>%access.logs%
-
-
-
-:Launcher_Main
-cls
-call keanu.date.time >nul
-call locations >nul
-cls
-cd K:\
-Set User=Jey
-cls
-Echo KEANU (C) 2019 107_Studios
-Echo Fast_Keanu_Prompt
-call Keanu.Logic.Main.bat
+Set Loading=No
+Set Launch_Question=No
+Set MM=No
+Set Fast_Keanu=Yes
+Set Use_Admin=No
+Set Use_Pre_User=Yes
+Set Pre_User=Jey
+call Keanu.Launcher.bat
