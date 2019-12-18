3b6a3dda44cc76dbac363fa07f6d933ea8a87437
Admin368 <58757588+Admin368@users.noreply.github.com>
2019-12-18 18:24:04 +0800

    Variable marking error Missing "%" fix

--- Keanu.Logic.Main.Start.bat
+++ Keanu.Logic.Main.Start.bat
@@ -2,19 +2,19 @@
 cls
 Set Process.Name=keanu.logic.Main.Start
 call keanu.location.bat
-echo %keanu.date.time% User:%user started [Keanu.Logic.Main.Start] >>%access.logs%
+echo %keanu.date.time% User:%user% started [Keanu.Logic.Main.Start] >>%access.logs%
 goto :001254
 
 :001254
 cls
 color 16
-echo %keanu.date.time% User:%user calling [keanu.logo.bat] >>%access.logs%
+echo %keanu.date.time% User:%user% calling [keanu.logo.bat] >>%access.logs%
 call keanu.logo.bat
 REM 2>>%access.logs%
 
-echo %keanu.date.time% User:%user calling [keanu.version.bat] >>%access.logs%
+echo %keanu.date.time% User:%user% calling [keanu.version.bat] >>%access.logs%
 call keanu.version.bat
 REM 2>>%access.logs%
 
-echo %keanu.date.time% User:%user calling [keanu.Logic.Main.bat] >>%access.logs%
+echo %keanu.date.time% User:%user% calling [keanu.Logic.Main.bat] >>%access.logs%
 keanu.Logic.Main.bat
