25f3474ef3031b197f812d7dd978414da977f270
Admin368_Dg <58757588+Admin368@users.noreply.github.com>
2020-02-15 03:11:26 +0800

    change of *ssid*#ip_list to #ip_list#ssid#

---
+++ #ip_List#The_Tower#.bat
@@ -0,0 +1,10 @@
+#ip_List
+Set Router=192.168.1.1
+Set Dagi-Pc=192.168.1.102
+Set RM3=192.168.1.104
+Set South_iphone=192.168.1.110
+Set huawei_g9=192.168.1.105
+Set Jey_Pc=192.168.108
+Set Legend-T3=192.168.169
+Set Ekeng_iphone=192.168.1.103
+Set Server68=192.168.1.168
