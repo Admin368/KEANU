1b7d9101e53e3071cf69c6da1dcfd7a8b46ddd4e
Admin368_Dg <58757588+Admin368@users.noreply.github.com>
2020-04-01 10:22:35 +0800

    ahmed passwoird crack

---
+++ Ahmed_Crack.bat
@@ -0,0 +1,146 @@
+@echo off
+Echo Process starting
+Cd /d E:\Space_X\HK_X\JTR
+#Goodmems_071018010619
+set /a N=0
+
+
+:SECTION1
+Echo %Date% Section1 [Task 1-4]
+Echo %Date% Section1 [Task 1-4] >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+Echo %Date% Format '[Gg]oodmems071018010619'
+Echo %Date% Format '[Gg]oodmems071018010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+goto Task%N%
+:Task0
+call :Before_Task
+Echo %Date% Format '[Gg]oodmems[_/-.]07[_/-.]10[_/-.]18[_/-.]01[_/-.]06[_/-.]19'
+Echo %Date% Format '[Gg]oodmems[_/-.]07[_/-.]10[_/-.]18[_/-.]01[_/-.]06[_/-.]19' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --mask='[Gg]oodmems[_/-.]07[_/-.]10[_/-.]18[_/-.]01[_/-.]06[_/-.]19' file\goodmems.rar_hash.txt --session=Task0
+call :After_Task
+:Task1
+call :Before_Task
+Echo %Date% Format '[Gg]oodmems071018010619'
+Echo %Date% Format '[Gg]oodmems071018010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar--mask='[Gg]oodmems071018010619' file\goodmems.rar_hash.txt --session=Task1
+call :After_Task
+:Task2
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS071018010619'
+Echo %Date% Format '[Gg]OODMEMS071018010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS071018010619' file\goodmems.rar_hash.txt --session=Task2
+call :After_Task
+:Task3
+call :Before_Task
+Echo %Date% Format '[Gg]oodmems0710201801062019'
+Echo %Date% Format '[Gg]oodmems0710201801062019' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]oodmems0710201801062019' file\goodmems.rar_hash.txt --session=Task3
+call :After_Task
+:Task4
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS0710201801062019'
+Echo %Date% Format '[Gg]OODMEMS0710201801062019' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS0710201801062019' file\goodmems.rar_hash.txt --session=Task4
+call :After_Task
+
+
+:SECTION2
+Echo %Date% Section2 [Task 5-8]
+Echo %Date% Section2 [Task 5-8] >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+:Task5
+call :Before_Task
+Echo %Date% Format '[Gg]oodmems[_/-.]071018010619'
+Echo %Date% Format '[Gg]oodmems[_/-.]071018010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar--mask='[Gg]oodmems[_/-.]071018010619' file\goodmems.rar_hash.txt --session=Task5
+call :After_Task
+:Task6
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS[_/-.]071018010619'
+Echo %Date% Format '[Gg]OODMEMS[_/-.]071018010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS[_/-.]071018010619' file\goodmems.rar_hash.txt --session=Task6
+call :After_Task
+:Task7
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS[_/-.]071018010619'
+Echo %Date% Format '[Gg]OODMEMS[_/-.]071018010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]oodmems[_/-.]0710201801062019' file\goodmems.rar_hash.txt --session=Task7
+call :After_Task
+:Task8
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS[_/-.]0710201801062019'
+Echo %Date% Format '[Gg]OODMEMS[_/-.]0710201801062019' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS[_/-.]0710201801062019' file\goodmems.rar_hash.txt --session=Task8
+call :After_Task
+
+:SECTION3
+Echo %Date% Section3 [Task 9-12]
+Echo %Date% Section3 [Task 9-12] >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+:Task9
+call :Before_Task
+Echo %Date% Format '[Gg]oodmems[_/-.]071018[_/-.]010619'
+Echo %Date% Format '[Gg]oodmems[_/-.]071018[_/-.]010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar--mask='[Gg]oodmems[_/-.]071018[_/-.]010619' file\goodmems.rar_hash.txt --session=Task9
+call :After_Task
+:Task10
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS[_/-.]071018[_/-.]010619'
+Echo %Date% Format '[Gg]OODMEMS[_/-.]071018[_/-.]010619' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS[_/-.]071018[_/-.]010619' file\goodmems.rar_hash.txt --session=Task10
+call :After_Task
+:Task11
+call :Before_Task
+Echo %Date% Format '[Gg]oodmems[_/-.]07102018[_/-.]01062019'
+Echo %Date% Format '[Gg]oodmems[_/-.]07102018[_/-.]01062019' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]oodmems[_/-.]07102018[_/-.]01062019' file\goodmems.rar_hash.txt --session=Task11
+call :After_Task
+:Task12
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS[_/-.]07102018[_/-.]01062019'
+Echo %Date% Format '[Gg]OODMEMS[_/-.]07102018[_/-.]01062019' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS[_/-.]07102018[_/-.]01062019' file\goodmems.rar_hash.txt --session=Task12
+call :After_Task
+
+:SECTION4
+Echo %Date% Section4 [Task 13-15]
+Echo %Date% Section4 [Task 13-15] >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+:Task13
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS[_/-.]07[_/-.]10[_/-.]18[_/-.]01[_/-.]06[_/-.]19'
+Echo %Date% Format '[Gg]OODMEMS[_/-.]07[_/-.]10[_/-.]18[_/-.]01[_/-.]06[_/-.]19' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS[_/-.]07[_/-.]10[_/-.]18[_/-.]01[_/-.]06[_/-.]19' file\goodmems.rar_hash.txt --session=Task13
+call :After_Task
+:Task14
+call :Before_Task
+Echo %Date% Format '[Gg]oodmems[_/-.]07[_/-.]10[_/-.]2018[_/-.]01[_/-.]06[_/-.]2019'
+Echo %Date% Format '[Gg]oodmems[_/-.]07[_/-.]10[_/-.]2018[_/-.]01[_/-.]06[_/-.]2019' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]oodmems[_/-.]07[_/-.]10[_/-.]2018[_/-.]01[_/-.]06[_/-.]2019' file\goodmems.rar_hash.txt --session=Task14
+call :After_Task
+:Task15
+call :Before_Task
+Echo %Date% Format '[Gg]OODMEMS[_/-.]07[_/-.]10[_/-.]2018[_/-.]01[_/-.]06[_/-.]2019'
+Echo %Date% Format '[Gg]OODMEMS[_/-.]07[_/-.]10[_/-.]2018[_/-.]01[_/-.]06[_/-.]2019' >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+john --format=rar --mask='[Gg]OODMEMS[_/-.]07[_/-.]10[_/-.]2018[_/-.]01[_/-.]06[_/-.]2019' file\goodmems.rar_hash.txt --session=Task15
+call :After_Task
+
+
+#========================================================================================================================================#
+:Before_Task
+(
+Echo Before_Task%N%
+Echo Starting Task%N%
+Echo Task%N% Starting >>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+if '%Log.Task%N%%'=='1' (
+Echo Task%N% Already Done
+Set /a N=%N%+1
+Echo Attempting Task%N%
+goto :Task%N%
+)
+)
+:After_Task
+(
+Task%N% completed>>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+Echo Set Log.Task%N%=1
+Set /a N=%N%+1
+goto :Task%N%
+Echo Moving to Task%N%>>E:\Space_X\HK_X\JTR\File\Master_Crack.log
+)
+#=========================================================================================================================================#
